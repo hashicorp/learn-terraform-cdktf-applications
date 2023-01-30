c3bf1ab9f21d0b1504e4d5454d27f5a8fb5c81f2
Mutahhir Hayat <mutahhir.hayat@hashicorp.com>
2023-01-30 13:54:59 +0100

    chore: convert first part of kubernetes config

--- cdktf-examples/java/src/main/java/com/mycompany/app/MainStack.java
+++ cdktf-examples/java/src/main/java/com/mycompany/app/MainStack.java
@@ -7,38 +7,40 @@ import java.util.Map;
 
 import com.hashicorp.cdktf.TerraformStack;
 
-import com.hashicorp.cdktf.providers.kubernetes.deployment.Deployment;
-import com.hashicorp.cdktf.providers.kubernetes.deployment.DeploymentMetadata;
-import com.hashicorp.cdktf.providers.kubernetes.deployment.DeploymentSpec;
+import com.hashicorp.cdktf.providers.kubernetes.deployment.*;
+import com.hashicorp.cdktf.providers.kubernetes.provider.*;
 
 public class MainStack extends TerraformStack {
     public MainStack(final Construct scope, final String id) {
         super(scope, id);
 
-        Deployment.Builder.create(this, "myapp")
-                .metadata((DeploymentMetadata) Map.of(
-                        "labels", Map.of(
-                                "app", "myapp",
-                                "component", "frontend",
-                                "environment", "dev"),
-                        "name", "myapp-frontend-dev"))
-                .spec((DeploymentSpec) Map.of(
-                        "replicas", "1",
-                        "selector", Map.of(
-                                "matchLabels", Map.of(
-                                        "app", "myapp",
-                                        "component", "frontend",
-                                        "environment", "dev")),
-                        "template", Map.of(
-                                "metadata", Map.of(
-                                        "labels", Map.of(
-                                                "app", "myapp",
-                                                "component", "frontend",
-                                                "environment", "dev")),
-                                "spec", Map.of(
-                                        "container", List.of(Map.of(
-                                                "image", "nginx:latest",
-                                                "name", "myapp-frontend-dev"))))))
-                .build();
+        new KubernetesProvider(this, "provider", KubernetesProviderConfig.builder()
+                .configPath("../../kubeconfig.yaml")
+                .build());
+
+        new Deployment(this, "deployment", DeploymentConfig.builder()
+                .metadata(DeploymentMetadata.builder()
+                        .labels(Map.of("app", "myapp", "component", "frontend", "environment", "dev"))
+                        .name("myapp")
+                        .build())
+                .spec(DeploymentSpec.builder()
+                        .replicas("1")
+                        .selector(DeploymentSpecSelector.builder()
+                                .matchLabels(Map.of("app", "myapp", "component", "frontend", "environment", "dev"))
+                                .build())
+                        .template(DeploymentSpecTemplate.builder()
+                                .metadata(DeploymentSpecTemplateMetadata.builder()
+                                        .labels(Map.of("app", "myapp", "component", "frontend", "environment", "dev"))
+                                        .build())
+                                .spec(DeploymentSpecTemplateSpec.builder()
+                                        .container(List.of(DeploymentSpecTemplateSpecContainer.builder()
+                                                .image("nginx:latest")
+                                                .name("frontend")
+                                                .build()))
+                                        .build())
+                                .build())
+                        .build())
+                .build());
+
     }
 }
