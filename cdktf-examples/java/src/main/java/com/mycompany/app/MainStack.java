4f0fb42ab56b8b89991b549ae2fe58208c69b3ad
Mutahhir Hayat <mutahhir.hayat@hashicorp.com>
2023-01-31 18:42:39 +0100

    chore: add app_backend

--- cdktf-examples/java/src/main/java/com/mycompany/app/MainStack.java
+++ cdktf-examples/java/src/main/java/com/mycompany/app/MainStack.java
@@ -1,6 +1,8 @@
 package com.mycompany.app;
 
 import java.io.File;
+import java.util.Map;
+
 import com.mycompany.constructs.*;
 import software.constructs.Construct;
 import com.hashicorp.cdktf.TerraformStack;
@@ -14,13 +16,24 @@ public class MainStack extends TerraformStack {
                                 .configPath(new File("../../kubeconfig.yaml").getAbsolutePath())
                                 .build());
 
+                SimpleKubernetesWebApp app_backend = new SimpleKubernetesWebApp(this, "app_backend",
+                                new SimpleKubernetesWebAppConfig()
+                                                .setImage("localhost:5000/nocorp-backend:latest")
+                                                .setReplicas(1)
+                                                .setApp("myapp")
+                                                .setComponent("backend")
+                                                .setEnvironment("dev")
+                                                .setPort(30002));
+
                 new SimpleKubernetesWebApp(this, "app_frontend", new SimpleKubernetesWebAppConfig()
                                 .setImage("localhost:5000/nocorp-frontend:latest")
                                 .setReplicas(3)
                                 .setApp("myapp")
                                 .setComponent("frontend")
                                 .setEnvironment("dev")
-                                .setPort(30001));
+                                .setPort(30001)
+                                .setEnv(Map.of("BACKEND_APP_URL",
+                                                String.format("http://localhost:%d", app_backend.config.port()))));
 
         }
 }
