26d6c70cf817c0d80ec62d28b601fd28ef896220
Mutahhir Hayat <mutahhir.hayat@hashicorp.com>
2023-01-31 19:01:18 +0100

    chore: Add test stack

--- cdktf-examples/java/src/main/java/com/mycompany/app/Main.java
+++ cdktf-examples/java/src/main/java/com/mycompany/app/Main.java
@@ -21,6 +21,22 @@ public class Main {
                         .setComponent("frontend")
                         .setEnvironment("dev")
                         .setPort(30001)));
+
+        new MyStack(app, "app-test", new MyStackConfig()
+                .setFrontend(new SimpleKubernetesWebAppConfig()
+                        .setImage("localhost:5000/nocorp-frontend:latest")
+                        .setReplicas(4)
+                        .setApp("myapp")
+                        .setComponent("frontend")
+                        .setEnvironment("test")
+                        .setPort(30003))
+                .setBackend(new SimpleKubernetesWebAppConfig()
+                        .setImage("localhost:5000/nocorp-backend:latest")
+                        .setReplicas(2)
+                        .setApp("myapp")
+                        .setComponent("backend")
+                        .setEnvironment("test")
+                        .setPort(30004)));
         app.synth();
     }
 }
