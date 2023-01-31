a6bd02105c4ace32ed7154ef022cf7872d0fce3a
Mutahhir Hayat <mutahhir.hayat@hashicorp.com>
2023-01-31 18:57:58 +0100

    chore: Add stack parameters

--- cdktf-examples/java/src/main/java/com/mycompany/app/Main.java
+++ cdktf-examples/java/src/main/java/com/mycompany/app/Main.java
@@ -1,11 +1,26 @@
 package com.mycompany.app;
 
 import com.hashicorp.cdktf.App;
+import com.mycompany.constructs.SimpleKubernetesWebAppConfig;
 
 public class Main {
     public static void main(String[] args) {
         final App app = new App();
-        new MyStack(app, "app");
+        new MyStack(app, "app", new MyStackConfig()
+                .setBackend(new SimpleKubernetesWebAppConfig()
+                        .setImage("localhost:5000/nocorp-backend:latest")
+                        .setReplicas(1)
+                        .setApp("myapp")
+                        .setComponent("backend")
+                        .setEnvironment("dev")
+                        .setPort(30002))
+                .setFrontend(new SimpleKubernetesWebAppConfig()
+                        .setImage("localhost:5000/nocorp-frontend:latest")
+                        .setReplicas(3)
+                        .setApp("myapp")
+                        .setComponent("frontend")
+                        .setEnvironment("dev")
+                        .setPort(30001)));
         app.synth();
     }
 }
